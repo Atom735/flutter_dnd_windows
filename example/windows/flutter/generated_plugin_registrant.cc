8b4a1495f428746d3f7148e4e3e9b90eefd5b45f
Atom735 <Atom735.ag47@gmail.com>
2021-05-11 14:13:29 +0300

    initial commit

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_dnd_windows/flutter_dnd_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterDndWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterDndWindowsPlugin"));
+}
---
+++ test/flutter_dnd_windows_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_dnd_windows/flutter_dnd_windows.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('flutter_dnd_windows');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await FlutterDndWindows.platformVersion, '42');
+  });
+}
